a23309f7ad15f8f69b51996be5c8e656fa95eca7
Christopher Tien phong Nguyen <cnguyen5413@hargreaves.cs.arizona.edu>
2017-09-11 09:01:08 -0700

    Gold Leader Edit

--- Hello.java
+++ Hello.java
@@ -12,13 +12,13 @@ public class Hello {
         
         // Red Leader should add a print statement that says "Lock S-foils in attack position." 
         // Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
-           System.out.print("Stabilize your rear deflectors... Watch for enemy for enemy fighters.")
+           System.out.print("Stabilize your rear deflectors... Watch for enemy for enemy fighters.");
     }
 
     public static void goldLeader() {
 
         // Gold Leader should add a print statement that says "Loosen up!"
-           System.out.print("Loosen up!")   
+           System.out.print("Loosen up!");
 
     }
 
